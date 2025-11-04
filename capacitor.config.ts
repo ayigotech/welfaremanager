2a8cacef04403b38593109f0014fc25f057e939f
ayitteyg <ayitteyg@yahoo.com>
2025-11-04 10:28:35 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ayigotech.welfaremanager',
+  appName: 'welfare-manager',
+  webDir: 'www'
+};
+
+export default config;
